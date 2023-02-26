4708c5a02d431ef77bb1ec8be88f8237732aa48d
leonblue <qwerty29544@gmail.com>
2023-02-27 00:46:27 +0300

    new project with gmsh started

--- src/Stationary_3d_acoustics_on_tetrahedron.py
+++ src/Stationary_3d_acoustics_on_tetrahedron.py
@@ -3,10 +3,30 @@ import numba as nb
 import gmsh
 import sys
 import json
+import os
 from VIEM.utils.gmsh_parser import GMSHParser
 
 
-if __name__ == "__main__":
-    parser = GMSHParser("/home/leonblue/PycharmProjects/Volume_Stationary_Acoustics_Python/resources/mesh/sphere.msh", 3)
+def compute_volumes_tetrahedron(array_vertexes):
+    array_vertexes_first = np.repeat(a=array_vertexes, repeats=[3, 0, 0, 0], axis=1)
+    array_vertexes_rest = array_vertexes_first[:, 1:, :]
+    array_vectors_rest_along_first = array_vertexes_rest - array_vertexes_first
+    volumes = (1/6) * np.linalg.det()
+    return 0
+
+def main():
+    print(os.getcwd())
+    parser = GMSHParser(file_path=os.path.join("..", "resources", "mesh", "sphere.msh"), dims=3)
     sphere = parser.get_numpy()
-    print(sphere)
+    print(sphere.shape)
+    print(np.repeat(sphere, [3, 0, 0, 0], 1))
+    return 0
+
+
+if __name__ == "__main__":
+    # main()
+    array_test = np.array([[[1, 2], [3, 4]], [[1, 3], [5, 6]]])
+    print(array_test)
+    print(np.repeat(array_test, [3, 0], 1))
+    print(np.apply_along_axis(np.linalg.det, 2, array_test))
+
